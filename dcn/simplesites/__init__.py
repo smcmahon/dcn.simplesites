c433b0ee0cea9639c010ee9d43ce9f49d8f91c6b
Steve McMahon <steve@dcn.org>
2013-05-08 16:02:33 -0700

    now skinning on traversal

--- dcn/simplesites/__init__.py
+++ dcn/simplesites/__init__.py
@@ -2,3 +2,8 @@ from zope.i18nmessageid import MessageFactory
 
 # Set up the i18n message factory for our package
 MessageFactory = MessageFactory('dcn.simplesites')
+
+available_skins = {
+    u'Flexible Layout': 'Sunburst Theme',
+    u'Fixed Layout': 'Plone Classic Theme',
+}
---
+++ dcn/simplesites/subscribers.py
@@ -0,0 +1,37 @@
+from Acquisition import aq_base
+from Products.Five.component import LocalSiteHook, HOOK_NAME
+from Products.SiteAccess.AccessRule import AccessRule
+from ZPublisher.BeforeTraverse import registerBeforeTraverse
+
+from dcn.simplesites import available_skins
+
+
+def siteModified(context, event):
+    """
+      objectModified event on simple siteModified.
+      this code from collective.editsiteswitcher.
+    """
+
+    """Add local site hook with before traverse event.
+
+    Check to see if the current object already has a LocalSiteHook
+    from Five registered. If not, then register one ourselves, going
+    around ``enableSite`` since we don't want to make this object a
+    full ``ISite``, but just get the ``BeforeTraverseEvent`` fired.
+    """
+    base_object = aq_base(context)
+    if hasattr(base_object, HOOK_NAME):
+        return
+    hook = AccessRule(HOOK_NAME)
+    registerBeforeTraverse(base_object, hook, HOOK_NAME, 1)
+    setattr(base_object, HOOK_NAME, LocalSiteHook())
+
+
+def applyTheme(context, event):
+    """
+        Apply skin to request as we traverse
+    """
+
+    skin = available_skins.get(context.page_structure)
+    if skin is not None:
+        context.changeSkin(skin, event.request)
