01313298395957dfc565a231c951dc88bfe1e2a6
Steve McMahon <steve@dcn.org>
2013-06-17 14:55:17 -0700

    Apply our browser layer only as we traverse into a simple site

--- dcn/simplesites/subscribers.py
+++ dcn/simplesites/subscribers.py
@@ -1,7 +1,14 @@
 from Acquisition import aq_base
 from Products.Five.component import LocalSiteHook, HOOK_NAME
 from Products.SiteAccess.AccessRule import AccessRule
+from zope.interface import directlyProvides, directlyProvidedBy
 from ZPublisher.BeforeTraverse import registerBeforeTraverse
+
+from plone.theme.layer import default_layers
+from plone.theme.interfaces import IDefaultPloneLayer
+
+from interfaces import IMyBrowserLayer
+
 
 def siteModified(context, event):
     """
@@ -16,6 +23,8 @@ def siteModified(context, event):
     around ``enableSite`` since we don't want to make this object a
     full ``ISite``, but just get the ``BeforeTraverseEvent`` fired.
     """
+
+    # import pdb; pdb.set_trace()
     base_object = aq_base(context)
     if hasattr(base_object, HOOK_NAME):
         return
@@ -26,9 +35,26 @@ def siteModified(context, event):
 
 def applyTheme(context, event):
     """
-        Apply skin to request as we traverse
+        Apply IMyBrowserLayer and skin to request as we traverse
     """
 
+    # Add our browserlayer to the request.
+    # Code from plone.theme mark_layer; generalized for
+    # non-skin layer
+    layer_ifaces = []
+    default_ifaces = []
+    # We need to make sure IDefaultPloneLayer comes after
+    # any other layers, even if they don't explicitly extend it.
+    for layer in directlyProvidedBy(event.request):
+        if layer in default_layers:
+            default_ifaces.append(layer)
+        else:
+            layer_ifaces.append(layer)
+    ifaces = [IMyBrowserLayer, ] + layer_ifaces + default_ifaces
+    directlyProvides(event.request, *ifaces)
+
+    # set the skin
     skin = context.page_structure
     if skin is not None:
         context.changeSkin(skin, event.request)
+
